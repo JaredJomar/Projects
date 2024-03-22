fc0f6c7683b8a731ca51fe659a72741a80c905f8
JaredJomar <jared.cruz@upr.edu>
2024-03-22 15:09:21 -0400

    Update Auto Clicker script to run every 2 seconds and add a delay between clicks
    * Updated `setInterval()` function to call the `clickOnElements` function every 2000 milliseconds (or 2 seconds)
    * Added a `sleep()` function that pauses the script for a specified amount of time between each click
    * Increased the value of `delayBetweenClicks` from 1 to 1000 (in milliseconds) to add a delay of 1 second between each click

--- TampermonkeyProjects/NotARobot/AutoNotARobotCliker.js
+++ TampermonkeyProjects/NotARobot/AutoNotARobotCliker.js
@@ -1,5 +1,6 @@
+
 // ==UserScript==
-// @name         Auto Click "I'm not a robot" and "play-box" button
+// @name         Auto Click "I'm not a robot"
 // @namespace    http://tampermonkey.net/
 // @version      0.3
 // @description  Automatically clicks the "I'm not a robot" checkbox and a button with the class "play-box" on websites
@@ -13,19 +14,30 @@
 (function () {
     'use strict';
 
-    const recaptchaIntervalId = setInterval(function () {
-        const recaptchaCheckbox = document.querySelector('#recaptcha-anchor');
-        if (recaptchaCheckbox) {
-            clearInterval(recaptchaIntervalId);
-            recaptchaCheckbox.click();
-        }
-    }, 500);
+    // Define the element selector and delay between clicks (in milliseconds)
+    var elementSelector = 'input[type="button"]';
+    var delayBetweenClicks = 1000; // in milliseconds
 
-    const playBoxIntervalId = setInterval(function () {
-        const playBoxButtons = document.querySelectorAll('.play-box');
-        if (playBoxButtons.length > 0) {
-            clearInterval(playBoxIntervalId);
-            playBoxButtons.forEach(button => button.click());
+    // Set up an interval to run every 2 seconds
+    setInterval(function () {
+        // Check if the start element exists on the page
+        if (document.getElementById("start") !== null) {
+            // Call the clickOnElements function with the appropriate selector and delay values
+            clickOnElements(elementSelector, delayBetweenClicks);
         }
-    }, 500);
+    }, 2000);
+
+    // Define the clickOnElements function that clicks on all elements that match a given selector
+    function clickOnElements(selector, delay) {
+        var elements = document.querySelectorAll(selector);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].click();
+            sleep(delay); // Add a delay between each click
+        }
+    }
+
+    // Define the sleep function that pauses the script for a specified amount of time
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 })();
