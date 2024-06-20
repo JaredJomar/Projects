a6da821287c66cf6a46294d772180e520e0ed704
JaredJomar <jared.cruz@upr.edu>
2024-06-20 16:32:53 -0400

    refactor: Update Twitch Enhancements script to version 0.2
    The Twitch Enhancements script has been updated to version 0.2. This update includes the following changes:
    
    - Added functionality to automatically claim drops
    - Improved code organization and readability in various functions
    - Implemented selective page reload for 'https://www.twitch.tv/drops/inventory' page every 15 minutes
    - Updated the `PRIME_REWARD_SELECTOR` constant to improve the selection of prime rewards

--- TampermonkeyProjects/Twitch/TwitchEnhancements.js
+++ TampermonkeyProjects/Twitch/TwitchEnhancements.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Twitch Enhancements
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Automatically claim channel points, enable theater mode, and claim prime rewards on https://gaming.amazon.com/
 // @author       JJJ
 // @match        https://www.twitch.tv/*
 // @match        https://gaming.amazon.com/*
+// @match        https://www.twitch.tv/drops/inventory*
 // @icon         https://th.bing.com/th/id/R.d71be224f193da01e7e499165a8981c5?rik=uBYlAxJ4XyXmJg&riu=http%3a%2f%2fpngimg.com%2fuploads%2ftwitch%2ftwitch_PNG28.png&ehk=PMc5m5Fil%2bhyq1zilk3F3cuzxSluXFBE80XgxVIG0rM%3d&risl=&pid=ImgRaw&r=0
 // @grant        none
 // @license      MIT
@@ -13,16 +14,21 @@
 
 (function () {
     'use strict';
-
+    // Constants
     const PLAYER_SELECTOR = '.video-player';
-    const THEATER_MODE_BUTTON_SELECTOR = 'button[aria-label="Theatre Mode (alt+t)"]';
+    const THEATER_MODE_BUTTON_SELECTOR = 'button[aria-label="Modo cine (alt+t)"]';
     const CLOSE_MENU_BUTTON_SELECTOR = 'button[aria-label="Close Menu"]';
     const CLOSE_MODAL_BUTTON_SELECTOR = 'button[aria-label="Close modal"]';
     const THEATER_MODE_CLASS = 'theatre-mode';
-    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
     const CLAIMABLE_BONUS_SELECTOR = '.claimable-bonus__icon';
+    const CLAIM_DROPS_SELECTOR = '[class="ScCoreButton-sc-ocjdkq-0 ScCoreButtonPrimary-sc-ocjdkq-1 ejeLlX eHSNkH"]';
+    const PRIME_REWARD_SELECTOR = 'span[data-a-target="tw-button-text"] p[data-a-target="buy-box_call-to-action-text"][title="Get in-game content"], p[data-a-target="buy-box_call-to-action-text"][title="Get game"]';
     let claiming = false;
 
+    // Check if MutationObserver is supported
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
+    // Function to click a button
     function clickButton(buttonSelector) {
         const observer = new MutationObserver((mutationsList, observer) => {
             for (let mutation of mutationsList) {
@@ -40,6 +46,7 @@
         observer.observe(document, { childList: true, subtree: true });
     }
 
+    // Function to enable theater mode
     function enableTheaterMode() {
         const player = document.querySelector(PLAYER_SELECTOR);
         if (player) {
@@ -51,6 +58,7 @@
         }
     }
 
+    // Function to hide the global menu
     function hideGlobalMenu() {
         const GLOBAL_MENU_SELECTOR = 'div.ScBalloonWrapper-sc-14jr088-0.eEhNFm';
         const globalMenu = document.querySelector(GLOBAL_MENU_SELECTOR);
@@ -61,18 +69,16 @@
         }
     }
 
+    // Function to automatically claim channel points
     function autoClaimBonus() {
         if (MutationObserver) {
             console.log('Auto claimer is enabled.');
 
-            // Create a new MutationObserver instance
             let observer = new MutationObserver(mutationsList => {
                 for (let mutation of mutationsList) {
                     if (mutation.type === 'childList') {
-                        // Check if there is a claimable bonus available
                         let bonus = document.querySelector(CLAIMABLE_BONUS_SELECTOR);
                         if (bonus && !claiming) {
-                            // Click on the bonus to claim it
                             bonus.click();
                             let date = new Date();
                             claiming = true;
@@ -85,18 +91,29 @@
                 }
             });
 
-            // Observe changes in the document body and its subtree
             observer.observe(document.body, { childList: true, subtree: true });
         } else {
             console.log('MutationObserver is not supported in this browser.');
         }
     }
 
+    // Function to claim prime rewards
     function claimPrimeReward() {
-        const element = document.querySelector('span[data-a-target="tw-button-text"] p[data-a-target="buy-box_call-to-action-text"][title="Get in-game content"]');
+        const element = document.querySelector(PRIME_REWARD_SELECTOR);
         if (element) {
             element.click();
         }
+    }
+
+    // Function to claim drops
+    function claimDrops() {
+        var onMutate = function (mutationsList) {
+            mutationsList.forEach(mutation => {
+                if (document.querySelector(CLAIM_DROPS_SELECTOR)) document.querySelector(CLAIM_DROPS_SELECTOR).click();
+            })
+        }
+        var observer = new MutationObserver(onMutate);
+        observer.observe(document.body, { childList: true, subtree: true });
     }
 
     setTimeout(enableTheaterMode, 1000);
@@ -105,5 +122,11 @@
     setTimeout(() => clickButton(CLOSE_MENU_BUTTON_SELECTOR), 1000);
     setTimeout(() => clickButton(CLOSE_MODAL_BUTTON_SELECTOR), 1000);
     setTimeout(hideGlobalMenu, 1000);
-}
-)();
+    setTimeout(claimDrops, 1000);
+
+    setInterval(function () {
+        if (window.location.href.startsWith('https://www.twitch.tv/drops/inventory')) {
+            window.location.reload();
+        }
+    }, 15 * 60000);
+})();
